10f61e69eed40b87a464908e94c99ec2b99603e9
Chris <chrischuck35@gmail.com>
2017-04-15 03:57:08 -0700

    Finishes shelter facing page

--- src/components/account/components/shelter.js
+++ src/components/account/components/shelter.js
@@ -1,11 +1,244 @@
 import { h, Component } from 'preact';
 import { Card, TextField, Button } from 'preact-mdl';
+import data from '../data/pastOrders';
 
 export default class Shelter extends Component {
+
+	componentWillMount() {
+		google.charts.load('current', {'packages':['corechart', 'bar']});
+	}
+
+	componentDidMount() {
+    	google.charts.setOnLoadCallback(this.drawChart);
+	}
+
+	drawChart = () => {
+		// create pie chart data
+		const pieData = new google.visualization.DataTable();
+        pieData.addColumn('string', 'Topping');
+        pieData.addColumn('number', 'Slices');
+        pieData.addRows([
+          ['Pad Thai', 3],
+          ['Yellow Curry', 1],
+          ['Broccoli Stirfry', 1]
+        ])
+	    const pieOptions = {'title':'Top overproduced foods',
+                       'width':'100%',
+                       'height': 220};
+		const pieChart = new google.visualization.PieChart(document.getElementById('analytics'));
+
+		const pieData2 = new google.visualization.DataTable();
+        pieData2.addColumn('string', 'Topping');
+        pieData2.addColumn('number', 'Slices');
+        pieData2.addRows([
+          ['Shelter 1', 2],
+          ['Shelter 2', 2],
+          ['Shelter 3', 1]
+        ])
+	    const pieOptions2 = {'title':'Distribution per Homeless Shelter',
+                       'width':'100%',
+                       'height': 220};
+		const pieChart2 = new google.visualization.PieChart(document.getElementById('analytics4'));
+
+		// create line chart
+		const lineData = google.visualization.arrayToDataTable([
+          ['Month', 'Appetizers', 'Entres'],
+          ['Jan',  300,      100],
+          ['Feb',  350,      130],
+          ['Mar',  400,       80],
+          ['Apr',  420,      100],
+		  ['May',  375,      150],
+		  ['June',  300,      200],
+		  ['July',  290,      240],
+		  ['Aug',  320,      180],
+		  ['Sep',  330,      230],
+		  ['Oct',  350,      350],
+		  ['Nov',  400,      300],
+		  ['Dec',  410,      260]
+        ]);
+
+        const lineOptions = {
+          title: 'Servings sold this year',
+          curveType: 'function',
+		  width: '100%',
+		  Height: 250,
+          legend: { position: 'bottom' }
+        };
+
+        const lineChart = new google.visualization.LineChart(document.getElementById('analytics2'));
+
+		// line 2 graph 
+		const lineData2 = google.visualization.arrayToDataTable([
+          ['Month', 'Dollars'],
+          ['Jan',  50],
+          ['Feb',  35],
+          ['Mar',  40],
+          ['Apr',  42],
+		  ['May',  37],
+		  ['June',  30],
+		  ['July',  29],
+		  ['Aug',  32],
+		  ['Sep',  33],
+		  ['Oct',  35],
+		  ['Nov',  40],
+		  ['Dec',  41]
+        ]);
+
+        const lineOptions2 = {
+          title: 'Tax Deductions',
+          curveType: 'function',
+		  width: '100%',
+		  Height: 250,
+          legend: { position: 'bottom' }
+        };
+
+        const lineChart2 = new google.visualization.LineChart(document.getElementById('analytics5'));
+
+		// create bar chart
+	    const barData = google.visualization.arrayToDataTable([
+			['Possible Lost vs. Gained Profit Comparison', 'Gained', 'Lost', { role: 'annotation' } ],
+			['Jan', 500, 700, ''],
+			['Feb', 350, 620, ''],
+			['Mar', 250, 400, ''],
+			['Apr', 500, 620, ''],
+			['May', 320, 390, ''],
+			['June', 400, 700, ''],
+			['July', 300, 450, ''],
+			['Aug', 370, 400, ''],
+			['Sep', 100, 300, ''],
+			['Oct', 450, 600, ''],
+			['Nov', 200, 370, ''],
+			['Dec', 270, 400, '']
+		]);
+
+		const barOptions = {
+          	isStacked: 'percent',
+         	height: 250,
+			width: '100%',
+          	legend: {position: 'left', maxLines: 3},
+          	vAxis: {
+            minValue: 0,
+            ticks: [0, .3, .6, .9, 1]
+          }
+        }
+		const barChart = new google.charts.Bar(document.getElementById('analytics3'));
+		// draw charts
+		
+		barChart.draw(barData, barOptions);
+    	pieChart.draw(pieData, pieOptions);
+		pieChart2.draw(pieData2, pieOptions2);
+		lineChart.draw(lineData, lineOptions);
+		lineChart2.draw(lineData2, lineOptions2);
+    }
+
+	renderItems = () => {
+		return data.map(i =>
+				<Card
+				style={{
+					boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)',
+					margin: 10,
+					position: 'relative',
+					width: '100%',
+					height: 100,
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					marginLeft: 0,
+					marginRight: 0
+				}}
+				>
+
+				<div
+				style={{
+					display: 'flex',
+					justifyContent: 'flex-start',
+					alignItems: 'center'
+				}}>
+					<h1 style={{marginBottom:15}}>
+						{i.name}
+					</h1>
+				</div>
+
+
+				</Card>
+		);
+	}
 	render() {
 		return (
 			<div>
-				
+				<div
+					style={{
+						display:'flex'
+					}}
+				>
+					<div
+					style={{
+						display: 'flex',
+						flexWrap: 'wrap',
+						flexDirection: 'column',
+						width: 400,
+						marginRight: 15,
+						textAlign: 'center'
+					}}>
+					<h1
+					>Past Deliveries</h1>
+					<div
+					style={{
+						maxHeight: '830',
+						height: 830,
+						overflow: 'scroll'
+					}}
+					>
+						{this.renderItems()}
+					</div>
+					</div>
+
+
+					<Card style={{
+						float:'right',
+						boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)',
+						width: '100%',
+						height: '100%'
+					}}
+						>
+						<div
+						style={{
+							display: 'flex',
+							flexWrap: 'wrap',
+							justifyContent: 'center'
+						}}
+						>	
+							<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center'
+							}}
+							>	
+								<div id='analytics'></div>
+								<div id='analytics4'></div>
+							</div>
+
+							<div
+							style={{
+								width: '90%'
+							}}
+							id='analytics3'></div>
+							<div
+							style={{
+								width: '100%'
+							}}
+							id='analytics2'></div>
+							<div
+							style={{
+								width: '100%'
+							}}
+							id='analytics5'></div>
+						</div>
+						
+
+					</Card>
+
+				</div>
 			</div>
 		);
 	}
--- src/components/account/index.js
+++ src/components/account/index.js
@@ -6,10 +6,15 @@ import Shelter from './components/shelter';
 
 export default class Account extends Component {
 	render() {
+		const jwt = localStorage.getItem("jwt");
 		return (
 			<div class={style.home} >
                 <div style={{ marginTop: '15px'}}>
-                    <Restaurant />
+                    { 
+					jwt === 'r' ?
+					<Restaurant /> :
+					<Shelter/>
+					}
                 </div>
 			</div>
 		);
