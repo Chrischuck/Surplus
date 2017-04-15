f76bfc4fa8319209fb18bdbe6a12ec594bf47c8c
Chris <chrischuck35@gmail.com>
2017-04-15 08:51:22 -0700

    Makes mock data use real names

--- src/components/account/data/pastOrders.js
+++ src/components/account/data/pastOrders.js
@@ -1,36 +1,36 @@
 export default [
     {
-        name: 'restaurant 1',
+        name: 'Berkeley Food & Housing Project',
         date: '1/31/17',
         servings: 70
     },
     {
-        name: 'restaurant 2',
+        name: 'Sacred Heart Community Service',
         date: '2/12/17',
         servings: 40
     },
     {
-        name: 'restaurant 3',
+        name: 'City Team Ministry Men\'s Shelter',
         date: '2/15/2017',
         servings: 50
     },
         {
-        name: 'restaurant 4',
+        name: 'City Team Ministry Men\'s Shelter',
         date: '2/20/2017',
         servings: 80
     },
         {
-        name: 'restaurant 5',
+        name: 'CityTeam Ministries San Jose',
         date: '2/24/17',
         servings: 50
     },
         {
-        name: 'restaurant 6',
+        name: 'Berkeley Food & Housing Project',
         date: '3/1/17',
         servings: 70
     },
         {
-        name: 'restaurant 7',
+        name: 'CityTeam Ministries San Jose',
         date: '3/10/17',
         servings: 40
     }
