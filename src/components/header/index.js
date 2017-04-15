2c8d2048d911721474217ce4b2216d846e1ec8af
Chris <chrischuck35@gmail.com>
2017-04-15 03:32:13 -0700

    Adds logout ability

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,67 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
+import { Dialog } from 'preact-mdl';
+import logo from './assets/pic.jpg';
 import style from './style';
 
 export default class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalOpen: false
+		};
+	}
+
+	componentDidMount () {
+		document.getElementById('body').addEventListener('click', this.toggleModal);
+  	}
+
+	componentWillUnmount () {
+		document.getElementById('body').removeEventListener('click', this.toggleModal)
+	}
+
+	toggleModal = event => {
+		event.stopPropagation();
+		this.setState({ isModalOpen: !this.state.isModalOpen });
+	}
+
+	logout = () => {
+		localStorage.removeItem("jwt");
+		window.location.href = 'login';
+	}
+
 	render() {
 		return (
 			<header class={style.header}>
 				<h1>Surplus</h1>
-
+				<nav>
+					<div
+					style={{
+						height: '50px',
+						width: '50px',
+						verticalAlign: 'center',
+						marginTop: 2
+					}}
+					onClick={this.toggleModal}
+					>
+						<img style={{maxHeight: '100%', maxWidth:'100%', borderRadius: '50%'}}src={logo} />
+					</div>
+					{
+						this.state.isModalOpen &&
+						<Dialog open={true}
+						style={{
+							marginRight: 0,
+							padding: 7,
+							width: '150px',
+							textAlign: 'center',
+							cursor: 'pointer'
+						}}
+						onClick={this.logout}
+						>
+						<h6 style={{margin:0, padding:0}}>Logout</h6>
+						</Dialog>
+					}
+				</nav>
 			</header>
 		);
 	}
