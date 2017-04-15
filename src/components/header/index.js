a4bcb09180be13aa5f2894ac9faf0e2ac14ad16d
Chris <chrischuck35@gmail.com>
2017-04-15 07:25:31 -0700

    Adds box shadow to profile picutre

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -48,7 +48,12 @@ export default class Header extends Component {
 					}}
 					onClick={this.toggleModal}
 					>
-						<img style={{maxHeight: '100%', maxWidth:'100%', borderRadius: '50%'}}src={logo} />
+						<img style={{
+							maxHeight: '100%',
+							maxWidth:'100%',
+							borderRadius: '50%',
+							boxShadow: '0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)'
+							}}src={logo} />
 					</div>
 					{
 						this.state.isModalOpen &&
